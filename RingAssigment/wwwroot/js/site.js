fbdd858c627a7fbca9dbe55e4608dc7516f17ef1
Andrew Rady <andrew.arsoftware@gmail.com>
2020-10-19 21:41:50 -0600

    adds custom js code for tabs

--- RingAssigment/wwwroot/js/site.js
+++ RingAssigment/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var tabs = document.querySelector('.nav-tabs')
+
+if (tabs) {
+    var children = Array.from(tabs.children);
+    children.forEach(function (tab) {
+        tab.addEventListener('click', function (e) {
+            e.preventDefault();
+            var activePaneValue = tab.textContent.trim().toLowerCase()
+            setActivePane(activePaneValue);
+            setActiveTab(tabs, tab);
+        })
+    })
+}
+
+function setActiveTab(tabs, currentTab) {
+    Array.from(tabs.children).forEach(function (x) {
+        x.children[0].classList.remove('active');
+    });
+    currentTab.children[0].classList.add('active');
+}
+
+function setActivePane(activePane) {
+    var content = document.querySelector('.tab-content');
+    var tabPane = Array.from(content.children);
+    tabPane.forEach(function (pane) {
+        pane.classList.remove('active')
+    })
+    document.getElementById(activePane).classList.add('active');
+
+}
